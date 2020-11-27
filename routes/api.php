64fcc10f8d0e7c055b13d81081fcd02f2563969c
Martin Halaj <m.halaj@websystem.sk>
2020-11-27 11:41:29 +0100

    Building the category model

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,3 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+
+    $categories = Category::parents()->ordered()->get();
+
+    dd($categories);
+
+});
---
+++ tests/Unit/Models/Categories/CategoryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit\Models\Categories;
+
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase
+{
+    public function test_it_many_children()
+    {
+        
+    }
+}
