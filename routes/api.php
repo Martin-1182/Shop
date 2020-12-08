32d18419fdac208dc1186be19ee901e87bbd8eda
Martin Halaj <m.halaj@websystem.sk>
2020-12-08 19:05:49 +0100

    The me endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\MeController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Products\ProductController;
@@ -26,6 +27,7 @@ Route::resource('products', ProductController::class);
 Route::group(['prefix' => 'auth'], function () {
         Route::post('register', [RegisterController::class, 'action']);
         Route::post('login', [LoginController::class, 'action']);
+        Route::get('me', [MeController::class, 'action']);
 });
 
 
